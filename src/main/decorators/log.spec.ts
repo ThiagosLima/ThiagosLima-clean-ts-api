aa3d49c589355599b232b92a90724cd09307389b
Thiago Lima <thiagolima.unb@gmail.com>
2020-09-21 16:20:59 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -11,6 +11,7 @@ const makeController = (): Controller => {
           name: 'Thiago'
         }
       }
+
       return await new Promise(resolve => resolve(httpResponse))
     }
   }
@@ -48,5 +49,25 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest)
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Thiago'
+      }
+    })
   })
 })
