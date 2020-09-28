65d037f3e2023363ee8fc2997c177767672e5804
Thiago Lima <thiagolima.unb@gmail.com>
2020-09-28 16:40:00 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/singup-validation.ts
+++ src/main/factories/singup-validation.ts
@@ -2,6 +2,8 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valid
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-filed-validation'
 import { Validation } from '../../presentation/helpers/validators/validaton'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fileds-validation'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -9,6 +11,7 @@ export const makeSignUpValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
   return new ValidationComposite(validations)
 }
