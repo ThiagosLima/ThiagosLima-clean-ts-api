d1f09c4b54c29020cef44f954050a26b68b82d79
Thiago Lima <thiagolima.unb@gmail.com>
2020-10-05 16:45:26 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/signup/singup-validation.ts
@@ -0,0 +1,17 @@
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-filed-validation'
+import { Validation } from '../../../presentation/helpers/validators/validaton'
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fileds-validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+  return new ValidationComposite(validations)
+}
